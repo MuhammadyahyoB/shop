100a7074cecaffc5e5eca463145f1e9f6de4103c
Muhammadyahyob <nabijonovmuhammad86@gmail.com>
2024-04-09 13:10:54 +0300

    first commit

---
+++ main/front/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'front'
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('product/<str:code>/', views.product_detail, name='product_detail'),
+    path('product_list', views.product_list, name='product_list1'),
+    path('category/<str:code>/', views.category_list, name='category_list'),
+
+    path('carts/', views.carts, name='carts'),
+    path('cart/<str:code>/', views.cart_detail, name='cart_detail'),
+    path('add-to-cart/<str:code>', views.add_to_cart, name='add_to_cart'),
+    path('active/cart/', views.active_cart, name='active_cart'),
+    path('cart_product_delete/<int:id>/', views.cart_product_delete, name='cart_product_delete'),
+    path('add_cart_product/<str:code>/', views.add_cart_product, name='add_cart_product'),
+
+    path('product_delete/<int:id>/',views.product_delete, name='product_delete'),
+
+    path('wishlist',views.list_wishlist, name='wishlist'),
+    path('wishlist_delete/<str:code>/', views.wishlist_delete, name='wishlist_delete'),
+    path('remove-wishlist/<str:code>',views.remove_wishlist, name='remove_wishlist'),
+    path('add-wishlist/<str:code>', views.add_wishlist, name='add_wishlist'),
+    path('order-list',views.order_list, name='order_list'),
+
+]
